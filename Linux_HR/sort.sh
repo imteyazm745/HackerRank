307c2954f92a8f0fce7883afdbbb6ff970af7184
Mohammad Imteyaz <52629525+imteyazm745@users.noreply.github.com>
2022-11-10 00:47:47 +0530

    Update sort.sh

--- Linux_HR/sort.sh
+++ Linux_HR/sort.sh
@@ -27,9 +27,59 @@ sort
 #when sort command is executed on the file without any option. 
 #It sorts lines in test file and displays sorted output.
 
-#Question -2 
+#Question 2 
 #order the lines in reverse lexicographical order (i.e. Z-A instead of A-Z).
 
 sort -r
 #The following sort command sorts lines in reverse order and displays sorted output.
 
+<<COMMENTS
+Question 3 :-
+You are given a text file where each line contains a number. The numbers may be either an integer or have decimal places. There will be no extra characters other than the number or the newline at the end of each line. 
+Sort the lines in ascending order - so that the first line holds the numerically smallest number, and the last line holds the numerically largest number.
+
+Sample Input
+9.1
+43.7
+2.2
+62.1
+2.1
+9.3
+43.5
+4.6
+44.6
+4.7
+42.7
+47.4
+46.6
+4.5
+55.6
+4
+9.2
+66.6
+2
+2.3
+
+Sample Output
+2
+2.1
+2.2
+2.3
+4
+4.5
+4.6
+4.7
+9.1
+9.2
+9.3
+42.7
+43.5
+43.7
+44.6
+46.6
+47.4
+55.6
+62.1
+66.6
+COMMENTS
+sort -n
