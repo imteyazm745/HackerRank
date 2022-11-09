24b3d141028b6a77d2a74f46efa497f23dcf62a4
Mohammad Imteyaz <52629525+imteyazm745@users.noreply.github.com>
2022-11-10 00:26:18 +0530

    Create sort1.sh

---
+++ Linux_HR/sort1.sh
@@ -0,0 +1,28 @@
+<<COMMENTS
+In this challenge, we practice using the sort command to sort input in text or TSV formats.
+Given a text file, order the lines in lexicographical order.
+
+Sample Input
+
+Dr. Rajendra Prasad     January 26, 1950    May 13, 1962
+Dr. S. Radhakrishnan        May 13, 1962    May 13, 1967
+Dr. Zakir Hussain       May 13, 1967    August 24, 1969
+Shri Varahagiri Venkata Giri        August 24, 1969 August 24, 1974
+Shri Fakhruddin Ali Ahmed       August 24, 1974 February 11, 1977
+Shri Neelam Sanjiva Reddy       July 25, 1977   July 25, 198
+
+Sample Output
+
+Dr. Rajendra Prasad     January 26, 1950    May 13, 1962
+Dr. S. Radhakrishnan        May 13, 1962    May 13, 1967
+Dr. Zakir Hussain       May 13, 1967    August 24, 1969
+Shri Fakhruddin Ali Ahmed       August 24, 1974 February 11, 1977
+Shri Neelam Sanjiva Reddy       July 25, 1977   July 25, 198
+Shri Varahagiri Venkata Giri        August 24, 1969 August 24, 1974
+
+COMMENTS
+
+sort 
+
+#when sort command is executed on the file without any option. 
+#It sorts lines in test file and displays sorted output.
