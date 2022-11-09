b41dc07af9dd1456462187ec414baf60acdfc301
Mohammad Imteyaz <52629525+imteyazm745@users.noreply.github.com>
2022-11-10 00:13:25 +0530

    Update tr.sh

--- Linux_HR/tr.sh
+++ Linux_HR/tr.sh
@@ -3,7 +3,8 @@
 
 tr "()" "[]"
 
-<<COMMENTSIn this challenge, we practice using the tr command because it is a useful translation tool in Linux.
+<<COMMENTS 
+In this challenge, we practice using the tr command because it is a useful translation tool in Linux.
 In a given fragment of text, delete all the lowercase characters a-z.
 
 Sample Input
