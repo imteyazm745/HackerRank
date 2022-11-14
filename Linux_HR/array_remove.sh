0192bcc0b2bea7bc82fa12d3cb1afdadd3bdc57f
Mohammad Imteyaz <52629525+imteyazm745@users.noreply.github.com>
2022-11-14 20:55:27 +0530

    Create array_remove.sh

---
+++ Linux_HR/array_remove.sh
@@ -0,0 +1,52 @@
+<<COMMENTS
+
+In this challenge, we practice reading and transforming arrays.
+
+Resources
+Here's a great tutorial with useful examples related to arrays in Bash.
+
+Task
+You are given a list of countries, each on a new line. Your task is to read them into an array and then transform them in the following way:
+
+The first capital letter (if present) in each element of the array should be replaced with a dot ('.'). Then, display the entire array with a space between each country's names.
+
+Input Format
+
+The input format consists of a list of country names each on a separate line. The only characters present in the country names will be upper or lower case characters and hyphens.
+
+Output Format
+
+Transform the names as described and display the entire array of country names with a space between each of them.
+
+Sample Input
+
+Namibia
+Nauru
+Nepal
+Netherlands
+NewZealand
+Nicaragua
+Niger
+Nigeria
+NorthKorea
+Norway
+Sample Output
+
+.amibia .auru .epal .etherlands .ewZealand .icaragua .iger .igeria .orthKorea .orway
+
+Explanation
+
+The first capital letter of each name has been replaced with a dot ('.').
+
+COMMENTS
+
+readarray y
+for i in ${y[@]}
+do
+echo -n ".${i:1} "
+done
+
+#OR
+
+#arr=($(cat))
+#echo ${arr[@]/[A-Z]/.}
