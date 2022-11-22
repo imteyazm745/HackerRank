553815fca1542c0d24d453fab6a8329bfd47ee0c
Mohammad Imteyaz <52629525+imteyazm745@users.noreply.github.com>
2022-11-22 21:28:13 +0530

    Update recursive_trees.sh

--- Linux_HR/recursive_trees.sh
+++ Linux_HR/recursive_trees.sh
@@ -1,1 +1,20 @@
 declare -A a
+# d - depth
+# l = length
+# r = row
+# c = column
+
+f() {
+    local d=$1 l=$2 r=$3 c=$4
+    [[ $d -eq 0 ]] && return
+    for ((i=l; i; i--)); do
+        a[$((r-i)).$c]=1
+    done
+    ((r -= l))
+    for ((i=l; i; i--)); do
+        a[$((r-i)).$((c-i))]=1
+        a[$((r-i)).$((c+i))]=1
+    done
+    f $((d-1)) $((l/2)) $((r-l)) $((c-l))
+    f $((d-1)) $((l/2)) $((r-l)) $((c+l))
+}
