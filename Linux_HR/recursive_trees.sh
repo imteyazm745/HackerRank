f77ed2955935302cfaed5a11e27f135f1d38ddce
Mohammad Imteyaz <52629525+imteyazm745@users.noreply.github.com>
2022-11-22 21:28:35 +0530

    Update recursive_trees.sh

--- Linux_HR/recursive_trees.sh
+++ Linux_HR/recursive_trees.sh
@@ -18,3 +18,15 @@ f() {
     f $((d-1)) $((l/2)) $((r-l)) $((c-l))
     f $((d-1)) $((l/2)) $((r-l)) $((c+l))
 }
+read n
+f $n 16 63 49
+for ((i=0; i<63; i++)); do
+    for ((j=0; j<100; j++)); do
+        if [[ ${a[$i.$j]} ]]; then
+            printf 1
+        else
+            printf _
+        fi
+    done
+    echo
+done
