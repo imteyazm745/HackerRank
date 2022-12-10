1abcb6da93c10dcec84e5e63cc6aa99409150e00
Mohammad Imteyaz <52629525+imteyazm745@users.noreply.github.com>
2022-12-10 23:37:46 +0530

    Update Hello_world.py

--- Python_HR/Hello_world.py
+++ Python_HR/Hello_world.py
@@ -9,11 +9,9 @@ print(my_string)
 The above code will print Hello, World! on your screen. Try it yourself in the editor below!
 
 Input Format
-
 You do not need to read any input in this challenge.
 
 Output Format
-
 Print Hello, World! to stdout.
 
 Sample Output 0
