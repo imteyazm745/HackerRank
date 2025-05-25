113b3d5be6b0513e6e591eff6bbed2d5aef5a56c
Mohammad Imteyaz <52629525+imteyazm745@users.noreply.github.com>
2025-05-25 06:51:44 +0530

    Update Revising_the_Select_Query_I.sql
    USING Aggregation function

--- SQL_HR/Revising_the_Select_Query_I.sql
+++ SQL_HR/Revising_the_Select_Query_I.sql
@@ -8,3 +8,14 @@ SELECT TRUNCATE(MAX(LAT_N), 4)
 FROM STATION
 WHERE LAT_N < 137.2345;
 
+-- Query the Western Longitude (LONG_W) for the largest Northern Latitude (LAT_N) in STATION that is less than . Round your answer to  decimal places.
+/*
+Enter your query here.
+*/
+SELECT ROUND(Long_W, 4)
+FROM STATION 
+WHERE LAT_N = (
+    SELECT MAX(LAT_N)
+    FROM STATION
+    WHERE LAT_N < 137.2345
+    );
