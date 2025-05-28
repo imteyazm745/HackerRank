45d062de3d5ff893778811ace121739c20f5b996
Mohammad Imteyaz <52629525+imteyazm745@users.noreply.github.com>
2025-05-28 22:17:04 +0530

    Update Coding_challenge.py

--- Python_HR/Coding_challenge.py
+++ Python_HR/Coding_challenge.py
@@ -14,3 +14,21 @@ else:
         print("Yes")
     else:
         print("No")
+
+
+# Write a program to print all numbers from 1 to n, but:
+# If the number is divisible by 3 → print "Fizz"
+# If the number is divisible by 5 → print "Buzz"
+# If divisible by both 3 and 5 → print "FizzBuzz"
+# Otherwise → print the number itself
+
+n = int(input())
+for i in range(1, n+1):
+    if i % 3 == 0 and n % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
