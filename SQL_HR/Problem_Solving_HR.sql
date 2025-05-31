514f0c43d2b79b6e203c62ea43cf439448a78c04
Mohammad Imteyaz <52629525+imteyazm745@users.noreply.github.com>
2025-05-31 10:31:04 +0530

    Update Problem_Solving_HR.sql

--- SQL_HR/Problem_Solving_HR.sql
+++ SQL_HR/Problem_Solving_HR.sql
@@ -48,3 +48,20 @@ SELECT
 FROM BST
 ORDER BY N;
 
+
+
+/*
+-- Consider P1(a,b) and P2(c,d) and  to be two points on a 2D plane.
+a happens to equal the minimum value in Northern Latitude (LAT_N in STATION).
+b happens to equal the minimum value in Western Longitude (LONG_W in STATION).
+c happens to equal the maximum value in Northern Latitude (LAT_N in STATION).
+d happens to equal the maximum value in Western Longitude (LONG_W in STATION).
+
+Query the Manhattan Distance between points  and  and round it to a scale of 4  decimal places.
+*/
+
+SELECT ROUND(
+    ABS((MIN(LAT_N) - MAX(LAT_N)) + (MIN(LONG_w) - MAX(LONG_w))), 4
+)
+FROM STATION;
+
