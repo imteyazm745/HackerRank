e7960f81b422ccdb662993401fd639e90fa478ed
Mohammad Imteyaz <52629525+imteyazm745@users.noreply.github.com>
2025-06-09 21:36:29 +0530

    Update Problem_Solving_HR.sql

--- SQL_HR/Problem_Solving_HR.sql
+++ SQL_HR/Problem_Solving_HR.sql
@@ -65,3 +65,27 @@ SELECT ROUND(
 )
 FROM STATION;
 
+
+
+"""
+Consider P1(a, c) and P2(b, d) to be two points on a 2D plane where (a,b) are the respective minimum and maximum values of Northern Latitude (LAT_N) and (c,d) are the respective minimum and maximum values of Western Longitude (LONG_W) in STATION.
+Query the Euclidean Distance between points P1 and P2 and format your answer to display 4 decimal digits.
+
+Input Format
+The STATION table is described as follows:
+
+LINK: https://www.hackerrank.com/challenges/weather-observation-station-19/problem?isFullScreen=true
+"""
+SELECT ROUND(
+  SQRT(
+    POWER(MAX(LAT_N) - MIN(LAT_N), 2) + 
+    POWER(MAX(lONG_W) - MIN(long_w), 2)
+  ),
+  4
+)
+FROM STATION
+
+
+
+
+
